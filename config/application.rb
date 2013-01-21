618a0f21aa4378b54bb4d3c60a4767a40f1f1b16
msyesyan <msyesyan@msyesyan.(none)>
2013-01-21 10:16:10 +0800

    修改application中assets的precompile配置

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,9 @@ module Prelude
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = true
+    
+    config.assets.precompile += ['bootstrap-responsive.min.css']
   end
 end
